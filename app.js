0fc2405aef40194b484135274a572877e69900b1
sahebjadi <sahebjadi786@gmail.com>
2026-06-23 23:32:27 +0530

    Add home route

--- app.js
+++ app.js
@@ -66,6 +66,9 @@ app.use((req,res,next)=>{
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
+app.get("/", (req,res)=>{
+    res.redirect("/listings");
+});
 app.use((req,res,next)=>{
   next(new ExpressError(404,"Page Not Found"))
 });
